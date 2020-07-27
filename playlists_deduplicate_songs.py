c312ab7031962dbecc85a0231e34e2ca4d6f04e9
Ben Haeringer <ben.haeringer@gmail.com>
2020-07-27 22:36:36 +0200

    Use native song structure + add playlist position

--- playlists_deduplicate_songs.py
+++ playlists_deduplicate_songs.py
@@ -22,37 +22,27 @@ def get_playlist_song_positions(playlist_id):
     playlist_dict = xmltodict.parse(playlist_xml)
     playlist = playlist_dict['subsonic-response']['playlist']
 
-    song_positions = {}
     for k, v in playlist.items():
-        if k == '@name':
-            song_positions['playlist_name'] = v
-        if k == '@id':
-            song_positions['playlist_id'] = v
-        if k == 'entry':
-            song_positions['songs'] = []
+        if k == 'entry':  # "entry" contains all entries of the playlist..
             for i, song in enumerate(v):
-                for k, v in song.items():
-                    if k == '@id':
-                        song_positions['songs'].append(
-                            {'position': i, 'song_id': v}
-                        )
-    return song_positions
+                song['position'] = i
+    return playlist
 
 
-def identify_duplicates(song_positions):
+def identify_duplicates(playlist):
     seen = {}
     pl_with_dups = {
-        'name': song_positions['playlist_name'],
-        'id': song_positions['playlist_id'],
+        'name': playlist['@name'],
+        'id': playlist['@id'],
         'dups': []
     }
-    for entry in song_positions['songs']:
-        sid = entry['song_id']
+    for song in playlist['entry']:
+        sid = song['@id']
         if sid not in seen:
             seen[sid] = 1
         else:
             if seen[sid] == 1:
-                pl_with_dups['dups'].append(entry)
+                pl_with_dups['dups'].append(song)
             seen[sid] += 1
     return pl_with_dups
 
@@ -62,5 +52,5 @@ if __name__ == "__main__":
     playlist_ids = get_playlist_ids()
 
     for plid in playlist_ids:
-        song_positions = get_playlist_song_positions(plid)
-        identify_duplicates(song_positions)
+        songs_with_positions = get_playlist_song_positions(plid)
+        playlist_with_duplicates = identify_duplicates(songs_with_positions)
